021e4b47d947c5c0c63520371cd524f9ee525237
fouomenedaniel <fouomenedaniel@gmail.com>
2020-12-16 12:55:32 +0100

    Ecriture des tests

---
+++ microservice/src/test/java/com/oc/microservice/dao/BookDaoTest.java
@@ -0,0 +1,13 @@
+package com.oc.microservice.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BookDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
